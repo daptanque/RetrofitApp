f968557dd6d4cf2bbfebf6a09979d9694d52f5f5
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-05 16:30:51 +0000

    Setup Retrofit on MainActivity GET

--- app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.retrofitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofitService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData : LiveData<Response<Albums>> =
+            liveData {
+                val response = retrofitService.getAlbums()
+                emit(response)
+            }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+
+            if(albumsList != null){
+                while(albumsList.hasNext()){
+                    val albumItem = albumsList.next()
+                    Log.i("TAGY", albumItem.title)
+                }
+            }
+        })
+
+
     }
 }
