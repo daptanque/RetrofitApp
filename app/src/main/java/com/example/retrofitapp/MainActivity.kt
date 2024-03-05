08ce418b6155a1bf8b95c60ac306bb13d9529c09
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-05 18:53:18 +0000

    Setting up for Query Parameters

--- app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.retrofitapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
@@ -13,14 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val text : TextView = findViewById(R.id.textview)
+
         val retrofitService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
 
         val responseLiveData : LiveData<Response<Albums>> =
             liveData {
-                val response = retrofitService.getAlbums()
-                emit(response)
+                //val response = retrofitService.getAlbums()
+
+                val response2 = retrofitService.getSpecificAlbums(1)
+                emit(response2)
             }
 
         responseLiveData.observe(this, Observer {
@@ -29,7 +34,12 @@ class MainActivity : AppCompatActivity() {
             if(albumsList != null){
                 while(albumsList.hasNext()){
                     val albumItem = albumsList.next()
-                    Log.i("TAGY", albumItem.title)
+                    //Log.i("TAGY", albumItem.title)
+
+                    val result = " Album Title: ${albumItem.title} \n"
+
+                    text.append(result)
+
                 }
             }
         })
